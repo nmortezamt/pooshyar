98a9b89e4a2df631d40ab583004408cf002c2f5d
nmortezamt <mortezanmt30@gmail.com>
2024-10-24 19:05:24 +0330

    major fix in namespaces Category, Product Models

--- Modules/Product/Color/Models/Color.php
+++ Modules/Product/Color/Models/Color.php
@@ -5,7 +5,7 @@ namespace Modules\Product\Color\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Modules\Product\Product\Models\product;
+use Modules\Product\Product\Models\Product;
 
 class Color extends Model
 {
